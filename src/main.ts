cc9c504ccd403816ba854856665ba49ca28ec796
GDevWeb <zodiaquestar@gmail.com>
2025-08-14 23:07:32 +0200

    feature(core): renderTable - rendering from JSON effective - step 2 completed. next step - sorting table by headers

--- src/main.ts
+++ src/main.ts
@@ -3,19 +3,13 @@ import type { Product } from "./types";
 import { renderTable } from "./ui/table";
 
 const productList: Product[] = products;
+type LoadProducts = Product[] | [];
 
-const productTable: HTMLTableElement | null =
-  document.querySelector("#productTable");
-
-export function loadProducts(list: Product[]): Product[] | [] | string {
+export function loadProducts(list: Product[]): LoadProducts {
   try {
     if (list.length === 0) {
       return [];
     }
-    if (productTable) {
-      renderTable(productTable, list);
-    }
-    console.log(list);
     return list;
   } catch (error) {
     console.error(error, "Error while loading product list");
@@ -24,4 +18,14 @@ export function loadProducts(list: Product[]): Product[] | [] | string {
     return list.length > 0 ? list : [];
   }
 }
-loadProducts(productList);
+
+/* ***init*** */
+const loadedProducts = loadProducts(productList);
+
+/* ***DOMElements*** */
+const productTable: HTMLTableElement | null =
+  document.querySelector("#productTable");
+
+if (productTable) {
+  renderTable(productTable, loadedProducts);
+}
--- src/ui/table.ts
+++ src/ui/table.ts
@@ -5,12 +5,56 @@ export function renderTable(
   data: Product[]
 ): void {
   try {
+    if (data.length === 0) {
+      container.innerHTML = "No product available for the moment";
+    }
+
     if (container && data) {
       console.log("renderTable running");
+      container.innerHTML = `
+        <thead class="bg-gray-50">
+          <tr class="text-center text-lg font-medium text-gray-500 uppercase tracking-wider">
+            <th px-6 py-3>ID</th>
+            <th px-6 py-3>Name</th>
+            <th px-6 py-3>Category</th>
+            <th px-6 py-3>Price</th>
+            <th px-6 py-3>Rating</th>
+            <th px-6 py-3>Stock</th>
+          </tr>
+        </thead>
+        <tbody>
+          ${data
+            .map(
+              (item) => `
+            <tr class="hover:bg-gray-100">
+              <td class="text-center px-6 py-4 whitespace-nowrap">${
+                item.id
+              }</td>
+              <td class="text-center px-6 py-4 whitespace-nowrap">${
+                item.name
+              }</td>
+              <td class="text-center px-6 py-4 whitespace-nowrap">${
+                item.category
+              }</td>
+              <td class="text-center px-6 py-4 whitespace-nowrap">${
+                item.price.toFixed(2) + "€"
+              }</td>
+              <td class="text-center px-6 py-4 whitespace-nowrap">${
+                item.rating
+              }</td>
+              <td class="text-center px-6 py-4 whitespace-nowrap">${
+                item.stock
+                  ? `<span class="text-green-500">available</span>`
+                  : `<span class="text-red-500">out of stock</span>`
+              }</td>
+            </tr>
+          `
+            )
+            .join("")}
+        </tbody>
+      `;
     }
   } catch (error) {
     console.error(error, "Error while rendering table");
   }
 }
-
-/* test */
