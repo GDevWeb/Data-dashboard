fc3d336c55902f1f25e908623deb8e7e39985c29
GDevWeb <zodiaquestar@gmail.com>
2025-08-15 17:04:06 +0200

    feature(sort): step 3rd - completed - hanle sorting by price and rating - asc & desc

--- src/core/state.ts
+++ src/core/state.ts
@@ -1,1 +1,9 @@
- 
+import type { Product, SortKey } from "../types";
+
+export function state(
+  data: Product[],
+  sortKey: SortKey,
+  sortDir: "asc" | "desc"
+): void {
+  console.log(data, sortKey, sortDir);
+}
